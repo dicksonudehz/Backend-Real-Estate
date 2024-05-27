22a97386b71bd5195e6ccd5a2b778faca721badb
MAC <mac@Dricks-Boy.local>
2024-05-27 12:48:38 +0100

    audience update

--- config/authconfig.js
+++ config/authconfig.js
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-  audience: "http://localhost:8000",
+  audience: 'this is the unique identifier',
   issuerBaseURL: 'https://dev-m2f3ppmzvnre585o.us.auth0.com/',
   tokenSigningAlg: 'RS256'
 });
