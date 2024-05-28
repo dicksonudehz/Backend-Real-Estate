d0208a7c3ccf1bb4ecad3e580ab3975d7b5c7382
MAC <mac@Dricks-Boy.local>
2024-05-28 05:10:44 +0100

    audience updated

--- config/authconfig.js
+++ config/authconfig.js
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-  audience: 'http://localhost:8000',
+  audience: 'https://backend-real-estate-six.vercel.app',
   issuerBaseURL: 'https://dev-m2f3ppmzvnre585o.us.auth0.com/',
   tokenSigningAlg: 'RS256'
 });
